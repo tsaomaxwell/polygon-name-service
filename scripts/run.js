d4657d44a204742f3f233edaa8ef75b8fc307c81
tsaomaxwell <68205375+tsaomaxwell@users.noreply.github.com>
2022-03-05 18:16:18 -0800

    string processing and payment

--- scripts/run.js
+++ scripts/run.js
@@ -1,23 +1,20 @@
 const main = async () => {
-    // The first return is the deployer, the second is a random account
-    const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-    const domainContract = await domainContractFactory.deploy();
+    // We pass in "ninja" to the constructor when deploying
+    const domainContract = await domainContractFactory.deploy("listenlistenlisten");
     await domainContract.deployed();
+  
     console.log("Contract deployed to:", domainContract.address);
-    console.log("Contract deployed by:", owner.address);
   
-    let txn = await domainContract.register("blues");
+    // We're passing in a second variable - value. This is the moneyyyyyyyyyy
+    let txn = await domainContract.register("mortal",  {value: hre.ethers.utils.parseEther('0.1')});
     await txn.wait();
   
-    const domainAddress = await domainContract.getAddress("blues");
-    console.log("Owner of domain blues:", domainAddress);
-    txn = await domainContract.connect(domainAddress).setRecord("blues", "https://voca.ro/1ktIPreupoYb");
-    await txn.wait();
+    const address = await domainContract.getAddress("mortal");
+    console.log("Owner of domain mortal:", address);
   
-    // Trying to set a record that doesn't belong to me!
-    txn = await domainContract.connect(randomPerson).setRecord("blues", "https://voca.ro/1dQ05pISuL30");
-    await txn.wait();
+    const balance = await hre.ethers.provider.getBalance(domainContract.address);
+    console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
   }
   
   const runMain = async () => {
