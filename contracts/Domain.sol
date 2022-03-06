d002f28a9d72125adf6972a9f432ba4dc1d2a56c
tsaomaxwell <68205375+tsaomaxwell@users.noreply.github.com>
2022-03-05 18:03:35 -0800

    domain name setup

--- contracts/Domain.sol
+++ contracts/Domain.sol
@@ -5,7 +5,29 @@ pragma solidity ^0.8.10;
 import "hardhat/console.sol";
 
 contract Domains {
-  constructor() {
-    console.log("THIS IS MY DOMAINS CONTRACT. NICE.");
-  }
+    mapping(string => address) public domains;
+    mapping(string => string) public records;
+
+    constructor() {
+        console.log("Contract made");
+    }
+
+    function register(string calldata name) public {
+        require(domains[name] == address(0));
+        domains[name] = msg.sender;
+        console.log("%s has registered a domain!", msg.sender);
+    }
+
+    function getAddress(string calldata name) public view returns (address) {
+        return domains[name];
+    }
+
+    function setRecord(string calldata name, string calldata record) public {
+        require(domains[name] == msg.sender);
+        records[name] = record;
+    }
+
+    function getRecord(string calldata name) public view returns(string memory) {
+        return records[name];
+    }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,9 +1,24 @@
 const main = async () => {
+    // The first return is the deployer, the second is a random account
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
     const domainContract = await domainContractFactory.deploy();
     await domainContract.deployed();
     console.log("Contract deployed to:", domainContract.address);
-  };
+    console.log("Contract deployed by:", owner.address);
+  
+    let txn = await domainContract.register("blues");
+    await txn.wait();
+  
+    const domainAddress = await domainContract.getAddress("blues");
+    console.log("Owner of domain blues:", domainAddress);
+    txn = await domainContract.connect(domainAddress).setRecord("blues", "https://voca.ro/1ktIPreupoYb");
+    await txn.wait();
+  
+    // Trying to set a record that doesn't belong to me!
+    txn = await domainContract.connect(randomPerson).setRecord("blues", "https://voca.ro/1dQ05pISuL30");
+    await txn.wait();
+  }
   
   const runMain = async () => {
     try {
